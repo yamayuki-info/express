4a99faa93db21f05fda6132a5af97c3273bcf939
yamayuki-info <info@yamayuki-info.co.jp>
2025-07-31 04:20:36 +0900

    commit

--- routes/notes.js
+++ routes/notes.js
@@ -1,21 +1,27 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
+const cors = require('cors'); // corsミドルウェアを追加
+require('dotenv').config();
+
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "****";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
-router.get("/", async (req, res) => {
-  // データベース、コレクション指定
-  const database = client.db("notes");
-  const notes = database.collection("notes");
+// corsミドルウェアを使用
+router.use(cors());
 
-  idが1のドキュメントを取得;
-  const query = { id: 1 };
-  const note = await notes.findOne(query);
+router.get('/', async (req, res) => {
+// データベース、コレクションを指定
+const database = client.db('notes');
+const notes = database.collection('notes');
 
-  res.json(note);
-});
+// idが１のドキュメントを取得
+const query = { id: 2 };
+const note = await notes.findOne(query);
+
+res.json(note);
+})
 
 module.exports = router;
